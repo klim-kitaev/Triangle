857d20387d9f96d0a83da506cdc7ba5b15756be6
Kitaev <kitaev@skrin.ru>
2015-11-30 11:26:30 +0300

    upload

---
+++ Triangle/RightTriangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    /// <summary>
+    /// Прямоугольный треугольник
+    /// </summary>
+    public class RightTriangle
+    {
+        //первый катет
+        private double a;
+        //второй катет
+        private double b;
+        //третий катет
+        private double c;
+
+        public RightTriangle(double s1, double s2, double s3)
+        {
+            //определим что является гипотенузой, а что катетами
+            double[] tmp=new double[]{s1,s2,s3}.OrderByDescending(p=>p).ToArray();
+            c = tmp[0];
+            a = tmp[1];
+            b = tmp[2];
+
+            //проверим на корркетность веденных данных
+            if (Math.Pow(a, 2) + Math.Pow(b, 2) != Math.Pow(c, 2))
+            {
+                throw new ArgumentException("Некорректные параметры сторон");
+            }
+        }
+        /// <summary>
+        /// Площадь прямоугольного треугольника
+        /// </summary>
+        public double Area
+        {
+            get { return a*b/2; }
+        }
+    }
+}
---
+++ TriangleTest/TriangleTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace TriangleTest
+{
+    [TestClass]
+    public class TriangleTest1
+    {
+        [TestMethod]
+        public void RightAreaTest()
+        {
+            RightTriangle triangle=new RightTriangle(3,4,5);
+
+            double area = triangle.Area;
+
+            Assert.AreEqual(6,area,"Right Area");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArgumentExceptionTest()
+        {
+            RightTriangle triangle = new RightTriangle(3, 4, 6);
+        }
+    }
+}
